090c89e6fa81470945c676a8ac1c04480d20bc5f
Cyril Gravelier <magiccyril@gmail.com>
2016-12-15 14:47:43 +0000

    Message & form styling + javascript

--- themes/mariage/js/app.js
+++ themes/mariage/js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation();
+(function ($, Drupal) {
+  
+  'use strict';
+      
+  Drupal.behaviors.mariage = {
+    attach: function (context) {
+      // Smooth scrolling for scroll links
+      $('.btn-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+          || location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 500);
+            return false;
+          }
+        }
+      });
+    }
+  }
+
+})(jQuery, Drupal);
